01af7964f42d11f45ffcd856a71741cb3ee57948
u2takey <wanglei2@qiniu.com>
2017-11-06 22:35:39 +0800

    xx

--- brigade.js
+++ brigade.js
@@ -7,3 +7,16 @@ events.on("exec", () => {
 events.on("push", () => {
   console.log(" **** I'm a GitHub 'push' handler")
 })
+
+events.on("exec", (e, p) => {
+  var one = new Job("one", "alpine:3.4")
+  var two = new Job("two", "alpine:3.4")
+
+  one.tasks = ["echo world"]
+  one.run().then( result => {
+    two.tasks = ["echo hello " + result.toString()]
+    two.run().then( result2 => {
+      console.log(result2.toString())
+    })
+  })
+})
